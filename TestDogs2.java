b21c844352fb138716cb6256dae06e70129c6368
Diskord <rickalodeon@gmail.com>
2015-10-21 17:07:16 -0400

    ¯\_(ツ)_/¯

---
+++ TestDogs2.java
@@ -0,0 +1,14 @@
+public class TestDogs2
+{
+	public static void main(String[] args)
+	{
+		DogTriathalonParticipant2 dog1 = new DogTriathalonParticipant2("Bowser", 2, 85, 89, 0);
+		dog1.display();
+		
+		DogTriathalonParticipant2 dog2 = new DogTriathalonParticipant2("Rush", 3, 78, 72, 80);
+		dog2.display();
+		
+		DogTriathalonParticipant2 dog3 = new DogTriathalonParticipant2("Ginger", 3, 90, 86, 72);
+		dog3.display();
+	}
+}
